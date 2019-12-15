cc4b60df5ef96724be79de2e49d6ea158c11dec6
Gubina Nataly <ntly.dutov@mail.ru>
2019-12-15 18:02:49 +0700

    second commit

---
+++ google.py
@@ -0,0 +1,32 @@
+import time
+
+# webdriver это и есть набор команд для управления браузером
+from selenium import webdriver
+
+# инициализируем драйвер браузера. После этой команды вы должны увидеть новое открытое окно браузера
+driver = webdriver.Chrome("C:\chromedriver\chromedriver.exe")
+
+# команда time.sleep устанавливает паузу в 3 секунд, чтобы мы успели увидеть, что происходит в браузере
+time.sleep(3)
+
+# Метод get сообщает браузеру, что нужно открыть сайт по указанной ссылке
+driver.get("https://google.ru/")
+time.sleep(3)
+
+# Метод find_element_by_css_selector позволяет найти нужный элемент на сайте, указав путь к нему. 
+# Ищем поле для ввода текста
+textinput = driver.find_element_by_css_selector(".gLFyf")
+
+# Напишем текст для поиска в найденное поле
+textinput.send_keys("Интерпретатор Python")
+time.sleep(3)
+
+# Найдем кнопку, которая отправляет введенный текст
+submit_button = driver.find_element_by_css_selector(".gNO89b")
+
+# Скажем драйверу, что нужно нажать на кнопку. 
+submit_button.click()
+time.sleep(10)
+
+# После выполнения всех действий мы не должны забыть закрыть окно браузера
+driver.quit()
